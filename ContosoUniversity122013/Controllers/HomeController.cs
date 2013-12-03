dcae0e010e2149cbb274fb676d13dd8d90e9db92
Paul Goldsmith <paul.goldsmith@spotlight.com>
2013-12-03 09:14:48 +0000

    welcome message added to homecontroller

--- ContosoUniversity122013/Controllers/HomeController.cs
+++ ContosoUniversity122013/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ContosoUniversity122013.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Welcome to Contoso University";
 
             return View();
         }
